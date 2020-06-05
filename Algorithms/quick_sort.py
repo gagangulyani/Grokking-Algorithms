553bb71cafdbec3d1af85983a880bcc1552a58b2
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-06-06 01:08:42 +0530

    Update quick_sort.py
    Update comment and move import statement

--- Algorithms/quick_sort.py
+++ Algorithms/quick_sort.py
@@ -1,25 +1,30 @@
 """
     Quick Sort
-        It uses first element in List as Pivot element
-        (from Grocking Algorithms)
+    (from Grokking Algorithms)
 """
-from random import randint
 
 
 def quick_sort(arr):
     if len(arr) < 2:
-        # No need to sort an List with 1 element
+        # No need to sort a List with 0 or 1 element
         return arr
 
+    # choosing first element in list as pivot element
     pivot = arr[0]
 
+    # store elements from list less than or equal to
+    # pivot element
     less = [i for i in arr[1:] if i <= pivot]
-    more = [i for i in arr[1:] if i > pivot]
 
-    return quick_sort(less) + [pivot] + quick_sort(more)
+    # store elements from list greater than pivot element
+    great = [i for i in arr[1:] if i > pivot]
+
+    return quick_sort(less) + [pivot] + quick_sort(great)
 
 
 if __name__ == '__main__':
+    from random import randint
+
     while True:
         # Generate list of random integer ranging from size 1 to 100000
         # containing elements ranging from -10000 to 10000
@@ -32,11 +37,11 @@ if __name__ == '__main__':
         # Python's Built In Sorting function
         result1 = sorted(arr)
         print('List Sorted with Built-in Algo!')
-        
+
         # Quick Sort
         result2 = quick_sort(arr)
         print('List Sorted with Quick!')
-        
+
         if result1 == result2:
             print('Okay!')
         else:
